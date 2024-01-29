d5b825d3c92bd8422dfe6d266f9a3c5e1c7f4c4c
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-29 07:24:26 +0000

    Refactor PhiConfig class in configuration_phi.py

--- moondream/phi/configuration_phi.py
+++ moondream/phi/configuration_phi.py
@@ -1,22 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-
-import math
-from typing import Optional
-
 from transformers import PretrainedConfig
-
+from typing import Optional
+import math
 
 class PhiConfig(PretrainedConfig):
-    """Phi configuration."""
-
     model_type = "phi-msft"
-    attribute_map = {
-        "max_position_embeddings": "n_positions",
-        "hidden_size": "n_embd",
-        "num_attention_heads": "n_head",
-        "num_hidden_layers": "n_layer",
-    }
 
     def __init__(
         self,
@@ -41,26 +30,35 @@ class PhiConfig(PretrainedConfig):
         pad_vocab_size_multiple: int = 64,
         gradient_checkpointing: bool = False,
         **kwargs
-    ) -> None:
-        self.vocab_size = int(
-            math.ceil(vocab_size / pad_vocab_size_multiple) * pad_vocab_size_multiple
+    ):
+        pad_vocab_size = math.ceil(vocab_size / pad_vocab_size_multiple) * pad_vocab_size_multiple
+        super().__init__(
+            vocab_size=pad_vocab_size,
+            n_positions=n_positions,
+            n_embd=n_embd,
+            n_layer=n_layer,
+            n_inner=n_inner,
+            n_head=n_head,
+            n_head_kv=n_head_kv,
+            activation_function=activation_function,
+            attn_pdrop=attn_pdrop,
+            embd_pdrop=embd_pdrop,
+            resid_pdrop=resid_pdrop,
+            layer_norm_epsilon=layer_norm_epsilon,
+            initializer_range=initializer_range,
+            pad_vocab_size_multiple=pad_vocab_size_multiple,
+            tie_word_embeddings=tie_word_embeddings,
+            gradient_checkpointing=gradient_checkpointing,
+            **kwargs
         )
-        self.n_positions = n_positions
-        self.n_embd = n_embd
-        self.n_layer = n_layer
-        self.n_inner = n_inner
-        self.n_head = n_head
-        self.n_head_kv = n_head_kv
         self.rotary_dim = min(rotary_dim, n_embd // n_head)
-        self.activation_function = activation_function
         self.flash_attn = flash_attn
         self.flash_rotary = flash_rotary
         self.fused_dense = fused_dense
-        self.attn_pdrop = attn_pdrop
-        self.embd_pdrop = embd_pdrop
-        self.resid_pdrop = resid_pdrop
-        self.layer_norm_epsilon = layer_norm_epsilon
-        self.initializer_range = initializer_range
-        self.gradient_checkpointing = gradient_checkpointing
 
-        super().__init__(tie_word_embeddings=tie_word_embeddings, **kwargs)
+    attribute_map = {
+        "max_position_embeddings": "n_positions",
+        "hidden_size": "n_embd",
+        "num_attention_heads": "n_head",
+        "num_hidden_layers": "n_layer",
+    }
