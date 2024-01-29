70c5358887477991d91241fd66b597a3afc44a33
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-29 09:40:45 +0000

    Refactor code to improve readability and maintainability

--- sample.py
+++ sample.py
@@ -1,41 +1,26 @@
-import re
 import argparse
-from threading import Thread
-from moondream import VisionEncoder, TextModel
 from PIL import Image
+from moondream import VisionEncoder, TextModel
 from huggingface_hub import snapshot_download
-from transformers import TextIteratorStreamer
 
-model_path = snapshot_download("vikhyatk/moondream1")
+def main(image_path, prompt):
+    model_path = snapshot_download("vikhyatk/moondream1")
+    vision_encoder = VisionEncoder(model_path)
+    text_model = TextModel(model_path)
+    image = Image.open(image_path)
+    image_embeds = vision_encoder(image)
 
-vision_encoder = VisionEncoder(model_path)
-text_model = TextModel(model_path)
+    if prompt is None:
+        while True:
+            question = input("> ")
+            print(text_model.answer_question(image_embeds, question))
+    else:
+        print(">", prompt)
+        print(text_model.answer_question(image_embeds, prompt))
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--image", type=str, required=True)
-parser.add_argument("--prompt", type=str, required=False)
-args = parser.parse_args()
-
-image = Image.open(args.image)
-image_embeds = vision_encoder(image)
-
-if args.prompt is None:
-    while True:
-        question = input("> ")
-
-        streamer = TextIteratorStreamer(text_model.tokenizer, skip_special_tokens=True)
-        generation_kwargs = dict(image_embeds=image_embeds, question=question, streamer=streamer)
-        thread = Thread(target=text_model.answer_question, kwargs=generation_kwargs)
-        thread.start()
-
-        buffer = ""
-        for new_text in streamer:
-            buffer += new_text
-            if not new_text.endswith("<") and not new_text.endswith("END"):
-                print(buffer, end="", flush=True)
-                buffer = ""
-        print(re.sub("<$", "", re.sub("END$", "", buffer)))
-else:
-    question = args.prompt
-    print(">", question)
-    print(text_model.answer_question(image_embeds, question))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", type=str, required=True)
+    parser.add_argument("--prompt", type=str, required=False)
+    args = parser.parse_args()
+    main(args.image, args.prompt)
