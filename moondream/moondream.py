093ad112f43f6c521a0f8e788b84f0363bacdfd7
Todor Arnaudov <twenkid@gmail.com>
2024-04-17 00:42:47 +0300

    max_new_tokens as a parameter to answer_question and default 256
    max_new_tokens is added as a parameter to answer_question() and default is set to 256 instead of 512. Reasons: 1) more control, 2) at least in test generations I found about 200 wasn't interrupted (100 was small) ; 3) in order to reduce the delay when the model enters in loops generating .............. and taking a lot of time. Also: can some sort of an interrupt be added, either with a callback like in GPT4All, also a constraint when capturing a repetition of tokens etc., repetition_penalty etc.? Can we add a streaming mode? Thanks.

--- moondream/moondream.py
+++ moondream/moondream.py
@@ -87,8 +87,9 @@ class Moondream(PreTrainedModel):
         image_embeds,
         question,
         tokenizer,
-        chat_history="",
-        result_queue=None,
+        chat_history="",        
+        result_queue=None,        
+        max_new_tokens=256
         **kwargs,
     ):
         prompt = f"<image>\n\n{chat_history}Question: {question}\n\nAnswer:"
@@ -96,7 +97,7 @@ class Moondream(PreTrainedModel):
             image_embeds,
             prompt,
             tokenizer=tokenizer,
-            max_new_tokens=512,
+            max_new_tokens=max_new_tokens=,
             **kwargs,
         )[0]
         cleaned_answer = answer.strip()
