aa1b6b618b00ef9dad4e201bc5f69a3aa68f4b16
vik <vikhyatk@gmail.com>
2024-07-22 14:34:23 -0700

    update release script

--- hf_release.py
+++ hf_release.py
@@ -5,17 +5,21 @@ from moondream.configuration_moondream import MoondreamConfig
 MoondreamConfig.register_for_auto_class()
 Moondream.register_for_auto_class("AutoModelForCausalLM")
 
+OUT_MODEL = "vikhyatk/moondream-next"
+CKPT_DIRS = []
+
+
+def get_ckpt(filename):
+    ckpts = [torch.load(f"{dir}/{filename}", map_location="cpu") for dir in CKPT_DIRS]
+    avg_ckpt = {key: sum(ckpt[key] for ckpt in ckpts) / len(ckpts) for key in ckpts[0]}
+    return avg_ckpt
+
+
 config = MoondreamConfig()
 model = Moondream(config)
-model.vision_encoder.encoder.load_state_dict(
-    torch.load("checkpoints/vision_encoder.s9001.pt", map_location="cpu")
-)
-model.vision_encoder.projection.load_state_dict(
-    torch.load("checkpoints/vision_projection.s9001.pt", map_location="cpu")
-)
-model.text_model.load_state_dict(
-    torch.load("checkpoints/text_model.s9001.pt", map_location="cpu")
-)
+model.vision_encoder.encoder.load_state_dict(get_ckpt("vision_encoder.final.pt"))
+model.vision_encoder.projection.load_state_dict(get_ckpt("vision_projection.final.pt"))
+model.text_model.load_state_dict(get_ckpt("text_model.final.pt"))
 model = model.to(dtype=torch.float16)
 
-model.push_to_hub("vikhyatk/moondream2", config=config)
+model.push_to_hub(OUT_MODEL, config=config)
