03b04cefa382b0453d3bd751e507ef410acee5c8
vik <vikhyatk@gmail.com>
2024-07-22 20:06:21 -0700

    add POPE eval

---
+++ moondream/eval/pope.py
@@ -0,0 +1,67 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+from tqdm import tqdm
+from .. import detect_device
+
+MODEL_ID = "vikhyatk/moondream2"
+REVISION = "2024-05-20"
+DEVICE, DTYPE = detect_device()
+
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_ID, revision=REVISION, trust_remote_code=True
+)
+moondream = AutoModelForCausalLM.from_pretrained(
+    MODEL_ID,
+    revision=REVISION,
+    trust_remote_code=True,
+    attn_implementation="flash_attention_2",
+    torch_dtype=DTYPE,
+    device_map={"": DEVICE},
+)
+moondream.eval()
+
+pope_dataset = load_dataset("vikhyatk/POPE", split="test")
+
+stats = {
+    "adversarial": (0, 0),
+    "popular": (0, 0),
+    "random": (0, 0),
+}
+for row in tqdm(pope_dataset):
+    image = row["image"]
+    enc_image = moondream.encode_image(image)
+    for split in ["adversarial", "popular", "random"]:
+        for qa in row[split]:
+            question = qa["question"]
+            answer = qa["answer"]
+            prompt = f"{question}. Answer yes or no."
+            model_answer = moondream.answer_question(enc_image, prompt, tokenizer)
+            if model_answer.lower() == answer.lower():
+                stats[split] = (stats[split][0] + 1, stats[split][1] + 1)
+            else:
+                stats[split] = (stats[split][0], stats[split][1] + 1)
+
+print(
+    "Adversarial:",
+    stats["adversarial"][0],
+    "/",
+    stats["adversarial"][1],
+    ":",
+    stats["adversarial"][0] * 100.0 / stats["adversarial"][1],
+)
+print(
+    "Popular:",
+    stats["popular"][0],
+    "/",
+    stats["popular"][1],
+    ":",
+    stats["popular"][0] * 100.0 / stats["popular"][1],
+)
+print(
+    "Random:",
+    stats["random"][0],
+    "/",
+    stats["random"][1],
+    ":",
+    stats["random"][0] * 100.0 / stats["random"][1],
+)
