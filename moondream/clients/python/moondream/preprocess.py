b5f87562373074fe05e7a59c5faf2104a95f82c9
vik <vikhyatk@gmail.com>
2024-10-13 22:15:14 -0700

    add onnx inference code

---
+++ moondream/clients/python/moondream/preprocess.py
@@ -0,0 +1,62 @@
+import numpy as np
+from typing import List, Tuple, Union
+from PIL import Image
+
+
+def im_resize(
+    image: Image.Image,
+    size: Tuple[int, int],
+    resample: int = Image.Resampling.BICUBIC,
+) -> Image.Image:
+    return image.resize(size, resample=resample)
+
+
+def normalize(
+    image: np.ndarray,
+    mean: List[float] = [0.5, 0.5, 0.5],
+    std: List[float] = [0.5, 0.5, 0.5],
+) -> np.ndarray:
+    """
+    Normalize an image array.
+    """
+    return (image - np.array(mean)) / np.array(std)
+
+
+def create_patches(image: Image.Image, image_patch_size=378) -> np.ndarray:
+    """
+    Split the given image into a variable number of patches depending upon its
+    resolution.
+    """
+    # Start off with the global patch.
+    patches = [im_resize(image, (image_patch_size, image_patch_size))]
+
+    # Find the closest resolution template.
+    res_templates = [(1, 2), (2, 1), (2, 2)]
+    im_width, im_height = image.size
+    max_dim = max(im_width, im_height)
+    if max_dim < image_patch_size * 1.4:
+        # If the image is already small, we just do a single patch that is a
+        # duplicate of the global patch. This creates a small amount of
+        # redundant computation now, but it is simpler and future-proofs us
+        # if/when we condition the vision encoder on the patch type.
+        patches.append(patches[0])
+    else:
+        aspect_ratio = im_width / im_height
+        res_template = min(
+            res_templates, key=lambda size: abs((size[1] / size[0]) - aspect_ratio)
+        )
+        # TODO: Actually implement patching... just going to put in the global
+        # patch for now to make progress on other aspects.
+        patches.append(patches[0])
+
+    return np.stack(
+        [
+            normalize(
+                (np.array(patch_img) / 255.0),
+                mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5],
+            ).transpose(2, 0, 1)
+            for patch_img in patches
+        ],
+        dtype=np.float16,
+    )
--- moondream/clients/python/moondream/vl.py
+++ moondream/clients/python/moondream/vl.py
@@ -1,13 +1,33 @@
+import onnx
+import onnxruntime as ort
+import numpy as np
 import os
+import tarfile
 
-from typing import List, Union
+from typing import Generator, List, Union, Optional, Dict, TypedDict
 from PIL import Image
+from dataclasses import dataclass
+from tokenizers import Tokenizer
 
-from .ggml.load import load_weights
+from .preprocess import create_patches
 
 
+@dataclass
 class EncodedImage:
-    pass
+    kv_caches: List[np.ndarray]
+
+
+SamplingSettings = TypedDict(
+    "SamplingSettings",
+    {
+        "max_tokens": int,
+        "streaming": bool,
+    },
+    total=False,
+)
+
+DEFAULT_MAX_TOKENS = 1024
+DEFAULT_STREAMING = False
 
 
 class Region:
@@ -15,22 +35,56 @@ class Region:
 
 
 class VL:
-    def __init__(self, model_path: str):
+    def __init__(self, model_path: Optional[str]):
         """
         Initialize the Moondream VL (Vision Language) model.
 
         Args:
-            model_path (str): The path to the model GGUF file.
+            model_path (str): The path to the model file.
 
         Returns:
             None
         """
-        if not os.path.isfile(model_path):
-            raise FileNotFoundError(f"Model file not found: {model_path}")
 
-        # Load the model weights
-        self.model = load_weights(model_path)
-        print(list(self.model.keys()))
+        if model_path is None or not os.path.isfile(model_path):
+            raise ValueError("Model path is invalid or file does not exist.")
+
+        if not tarfile.is_tarfile(model_path):
+            raise ValueError(
+                "Model format not recognized. You may need to upgrade the moondream package."
+            )
+
+        self.text_decoders = []
+
+        with tarfile.open(model_path, "r:*") as tar:
+            for member in tar.getmembers():
+                name = member.name
+
+                f = tar.extractfile(member)
+                if f is not None:
+                    contents = f.read()
+                else:
+                    continue
+
+                if name == "vision_encoder.onnx":
+                    self.vision_encoder = ort.InferenceSession(contents)
+                elif name == "vision_projection.onnx":
+                    self.vision_projection = ort.InferenceSession(contents)
+                elif name == "text_encoder.onnx":
+                    self.text_encoder = ort.InferenceSession(contents)
+                elif name == "tokenizer.json":
+                    self.tokenizer = Tokenizer.from_buffer(contents)
+                elif "text_decoder" in name and name.endswith(".onnx"):
+                    self.text_decoders.append(ort.InferenceSession(contents))
+
+        assert self.vision_encoder is not None
+        assert self.vision_projection is not None
+        assert self.tokenizer is not None
+        assert self.text_encoder is not None
+        assert len(self.text_decoders) > 0
+
+        self.eos_token_id = self.tokenizer.encode("<|endoftext|>").ids[0]
+        self.caption_prefix = self.tokenizer.encode("\n\nCaption:").ids
 
     def encode_image(self, image: Image.Image) -> EncodedImage:
         """
@@ -46,9 +100,34 @@ class VL:
         Returns:
             The encoded representation of the image.
         """
-        return EncodedImage()
+        (inputs_embeds_0,) = self.text_encoder.run(
+            None, {"input_ids": np.array([[self.eos_token_id]])}
+        )
+        image_patches = create_patches(image)
+        patch_emb = self.vision_encoder.run(None, {"input": image_patches})[0]
+        patch_emb = np.concatenate([patch_emb[0], patch_emb[1]], axis=-1)
+        patch_emb = np.expand_dims(patch_emb, axis=0)
+        (inputs_embeds_1,) = self.vision_projection.run(None, {"input": patch_emb})
+        inputs_embeds = np.concatenate([inputs_embeds_0, inputs_embeds_1], axis=1)
 
-    def caption(self, image: Union[Image.Image, EncodedImage]) -> str:
+        kv_caches = [
+            np.ndarray(
+                [24 // len(self.text_decoders), 2, 1, 32, 0, 64], dtype=np.float16
+            )
+            for _ in self.text_decoders
+        ]
+        for i, decoder in enumerate(self.text_decoders):
+            inputs_embeds, kv_caches[i] = decoder.run(
+                None, {"inputs_embeds": inputs_embeds, "kv_cache": kv_caches[i]}
+            )
+
+        return EncodedImage(kv_caches=kv_caches)
+
+    def caption(
+        self,
+        image: Union[Image.Image, EncodedImage],
+        settings: Optional[SamplingSettings] = None,
+    ) -> Union[str, Generator[str, None, None]]:
         """
         Generate a caption for the input image.
 
@@ -58,7 +137,39 @@ class VL:
         Returns:
             str: The caption for the input image.
         """
-        return "To be implemented."
+        if type(self.caption_prefix) == list:
+            (self.caption_prefix,) = self.text_encoder.run(
+                None, {"input_ids": [self.caption_prefix]}
+            )
+
+        if settings is None:
+            settings = {}
+        max_tokens = settings.get("max_tokens", DEFAULT_MAX_TOKENS)
+        streaming = settings.get("streaming", DEFAULT_STREAMING)
+
+        if type(image) != EncodedImage:
+            image = self.encode_image(image)  # type: ignore
+
+        hidden = self.caption_prefix
+        kv_caches = {i: image.kv_caches[i] for i in range(len(self.text_decoders))}
+        generated_tokens = []
+        while len(generated_tokens) < max_tokens:
+            for i, decoder in enumerate(self.text_decoders):
+                hidden, kv_caches[i] = decoder.run(
+                    None, {"inputs_embeds": hidden, "kv_cache": kv_caches[i]}
+                )
+
+            next_token = np.argmax(hidden, axis=-1)[0]
+            if next_token == self.eos_token_id:
+                break
+
+            if streaming:
+                yield self.tokenizer.decode([next_token])
+
+            generated_tokens.append(next_token)
+            (hidden,) = self.text_encoder.run(None, {"input_ids": [[next_token]]})
+
+        return self.tokenizer.decode(generated_tokens)
 
     def query(self, image: Union[Image.Image, EncodedImage], question: str) -> str:
         """
