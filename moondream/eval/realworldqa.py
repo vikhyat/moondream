2cb0035b9afafbbaff2a2b6f4cda00d43b5bcf71
vik <vikhyatk@gmail.com>
2025-01-04 09:25:33 -0800

    add chartqa, mmstar, realwordqa eval scripts

---
+++ moondream/eval/realworldqa.py
@@ -0,0 +1,51 @@
+import argparse
+import datasets
+import torch
+
+from tqdm import tqdm
+
+from ..torch.config import MoondreamConfig
+from ..torch.moondream import MoondreamModel
+from ..torch.weights import load_weights_into_model
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    if torch.cuda.is_available():
+        torch.set_default_device("cuda")
+    elif torch.backends.mps.is_available():
+        torch.set_default_device("mps")
+
+    config = MoondreamConfig()
+    model = MoondreamModel(config)
+    load_weights_into_model(args.model, model)
+
+    dataset = datasets.load_dataset("lmms-lab/RealWorldQA", split="test")
+
+    correct = 0
+    total = 0
+
+    for row in tqdm(dataset, disable=args.debug):
+        image = row["image"]
+        question = row["question"]
+        answer = row["answer"]
+        model_answer = model.query(image, question)["answer"]
+
+        total += 1
+        if model_answer.strip().lower() == answer.strip().lower():
+            correct += 1
+        elif args.debug:
+            print(f"Image: {row['image_path']}")
+            print(f"Question: {question}")
+            print(f"Answer: {answer}")
+            print(f"Model Answer: {model_answer}")
+        if args.debug:
+            print(f"Correct: {correct}, Total: {total}")
+            print(f"Accuracy: {correct * 100 / total:.2f}")
+            print("---------")
+
+    print(f"Correct: {correct}, Total: {total}")
+    print(f"Accuracy: {correct * 100 / total:.2f}")
