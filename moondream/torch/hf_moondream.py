ff0e3dc0342588aca801ad2bfb09b4a977bc12d8
vik <vikhyatk@gmail.com>
2025-01-11 14:14:53 -0800

    fix formatting error

--- moondream/torch/hf_moondream.py
+++ moondream/torch/hf_moondream.py
@@ -14,7 +14,7 @@ from .utils import *
 def extract_question(text):
     prefix = "<image>\n\nQuestion: "
     suffix = "\n\nAnswer:"
-    
+
     if text.startswith(prefix) and text.endswith(suffix):
         return text[len(prefix) : -len(suffix)]
     else:
@@ -98,22 +98,27 @@ class HfMoondream(PreTrainedModel):
         """
         prompt_extracted = extract_question(prompt)
         if prompt_extracted is not None:
-            answer = self.model.query(image=image_embeds, question=prompt_extracted, stream=False)[
-                "answer"
-            ]
+            answer = self.model.query(
+                image=image_embeds, question=prompt_extracted, stream=False
+            )["answer"]
         else:
             image_embeds = self.encode_image(image_embeds)
             prompt_tokens = torch.tensor(
                 [self.model.tokenizer.encode(prompt).ids],
                 device=self.device,
             )
+
             def generator():
                 for token in self.model._generate_text(
-                    prompt_tokens, image_embeds.kv_cache, image_embeds.pos, max_new_tokens
+                    prompt_tokens,
+                    image_embeds.kv_cache,
+                    image_embeds.pos,
+                    max_new_tokens,
                 ):
                     yield token
+
             answer = "".join(list(generator()))
-            
+
         return [answer]
 
     def get_input_embeddings(self):
