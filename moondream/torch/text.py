0c674a9185601d0c9d6c458376401c8091f8cf11
EthanReid <erpreid@gmail.com>
2025-02-06 16:54:01 -0500

    fix

--- moondream/torch/text.py
+++ moondream/torch/text.py
@@ -14,12 +14,12 @@ def text_encoder(input_ids: torch.Tensor, w: nn.Module):
 
 def attn(
     x: torch.Tensor,
-    w,
+    w: nn.Module,
     freqs_cis: torch.Tensor,
-    layer_kv_cache: torch.Tensor,
+    kv_cache: nn.Module,
     attn_mask: torch.Tensor,
     n_heads: int,
-    pos: int,
+    position_ids: torch.Tensor,
 ):
     bsz, q_len, d_model = x.shape
     head_dim = d_model // n_heads
@@ -29,14 +29,11 @@ def attn(
         for t in linear(x, w.qkv).chunk(3, dim=-1)
     ]
 
-    position_ids = torch.arange(pos, pos + q_len, dtype=torch.long)
     q = apply_rotary_emb(q, freqs_cis, position_ids, n_heads)
     k = apply_rotary_emb(k, freqs_cis, position_ids, n_heads)
 
-    k_, v_ = k, v
-    if layer_kv_cache is not None:
-        k = torch.cat([layer_kv_cache[0, :, :, :pos, :], k], dim=2)
-        v = torch.cat([layer_kv_cache[1, :, :, :pos, :], v], dim=2)
+    if kv_cache is not None:
+        k, v = kv_cache.update(position_ids, k, v)
 
     out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask).to(
         # This type conversion isn't needed when running in PyTorch directly, but the
@@ -59,76 +56,17 @@ def _attn(
     bsz, q_len, d_model = x.shape
     head_dim = d_model // n_heads
     pos = 0
-
     q, k, v = [
         t.view(bsz, q_len, n_heads, head_dim).transpose(1, 2)
         for t in w.qkv(x).chunk(3, dim=-1)
     ]
-
     position_ids = torch.arange(pos, pos + q_len, dtype=torch.long)
     q = apply_rotary_emb(q, freqs_cis, position_ids, n_heads)
     k = apply_rotary_emb(k, freqs_cis, position_ids, n_heads)
-
     out = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask)
     out = out.transpose(1, 2).reshape(bsz, q_len, d_model)
     out = linear(out, w.proj)
     return out
-
-
-def text_decoder(
-    inputs_embeds: torch.Tensor,
-    w: nn.Module,
-    kv_cache: torch.Tensor,
-    attn_mask: torch.Tensor,
-    pos: int,
-    config: TextConfig,
-):
-    hidden_BTC = inputs_embeds
-    new_kv_cache = [torch.empty(0)] * len(w.blocks)
-
-    for i, block in enumerate(w.blocks):
-        l_in = layer_norm(hidden_BTC, block.ln)
-        l_attn, new_kv_cache[i] = attn(
-            l_in,
-            block.attn,
-            freqs_cis=w.freqs_cis,
-            layer_kv_cache=kv_cache[i],
-            attn_mask=attn_mask,
-            n_heads=config.n_heads,
-            pos=pos,
-        )
-        l_mlp = mlp(l_in, block.mlp)
-        hidden_BTC = hidden_BTC + l_attn + l_mlp
-
-    return hidden_BTC, torch.stack(new_kv_cache)
-
-
-def lm_head(hidden_BTC: torch.Tensor, w: nn.Module):
-    hidden_BC = hidden_BTC[:, -1, :]
-    hidden_BC = layer_norm(hidden_BC, w.post_ln)
-    logits = linear(hidden_BC, w.lm_head)
-    return logits
-
-
-def _lm_head(hidden_BTC: torch.Tensor, w: nn.Module):
-    hidden_BTC = layer_norm(hidden_BTC, w.post_ln)
-    logits = linear(hidden_BTC, w.lm_head)
-    return logits
-
-
-def prefill(
-    inputs_embeds: torch.Tensor,
-    kv_cache: torch.Tensor,
-    attn_mask: torch.Tensor,
-    pos: int,
-    w: nn.Module,
-    config: TextConfig,
-):
-    # Updates kv_cache in-place
-    hidden, kv_cache[:, :, :, :, pos : pos + inputs_embeds.size(1), :] = text_decoder(
-        inputs_embeds, w, kv_cache, attn_mask, pos, config
-    )
-    return hidden
 
 
 def _produce_hidden(inputs_embeds: torch.Tensor, w: nn.Module, config: TextConfig):
@@ -156,19 +94,40 @@ def _produce_hidden(inputs_embeds: torch.Tensor, w: nn.Module, config: TextConfi
     return hidden_BTC
 
 
-def decode_one_token(
-    token_emb: torch.Tensor,
-    kv_cache: torch.Tensor,
-    attn_mask: torch.Tensor,
-    pos: int,
+def text_decoder(
+    x: torch.Tensor,
     w: nn.Module,
+    attn_mask: torch.Tensor,
+    position_ids: torch.Tensor,
     config: TextConfig,
 ):
-    hidden, kv_cache_update = text_decoder(
-        token_emb[None], w, kv_cache, attn_mask, pos, config
-    )
-    logits = lm_head(hidden, w)
-    return logits, hidden, kv_cache_update
+    for i, block in enumerate(w.blocks):
+        l_in = layer_norm(x, block.ln)
+        l_attn = attn(
+            l_in,
+            block.attn,
+            freqs_cis=w.freqs_cis,
+            kv_cache=block.kv_cache,
+            attn_mask=attn_mask,
+            n_heads=config.n_heads,
+            position_ids=position_ids,
+        )
+        l_mlp = mlp(l_in, block.mlp)
+        x = x + l_attn + l_mlp
+
+    return x
+
+
+def lm_head(hidden_BTC: torch.Tensor, w: nn.Module):
+    hidden_BC = hidden_BTC[:, -1, :]
+    hidden_BC = layer_norm(hidden_BC, w.post_ln)
+    logits = linear(hidden_BC, w.lm_head)
+    return logits
+
+def _lm_head(hidden_BTC: torch.Tensor, w: nn.Module):
+    hidden_BTC = layer_norm(hidden_BTC, w.post_ln)
+    logits = linear(hidden_BTC, w.lm_head)
+    return logits
 
 
 def build_text_model(config: TextConfig, dtype: torch.dtype) -> nn.Module:
